d7df8387fbc6b9af992e747f9c50c64b14db33ba
amir <amiram279@.012.net.il>
2015-07-06 12:47:44 +0300

    initial

---
+++ node-flat/DirUtils.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace node_flat
+{
+  public delegate void Command(DirectoryInfo[] dirs);
+  public class DirUtils
+  {
+    public void Walk(DirectoryInfo rootDir, Command cmd, string filter)
+    {
+      var dirs = rootDir.GetDirectories(filter);
+
+      if(dirs.Count() == 1)
+      {
+        var filteredDirectory = dirs[0];
+        var filteredDirectoryInnerDirs = filteredDirectory.GetDirectories();
+        cmd(filteredDirectoryInnerDirs);
+        foreach(var d in filteredDirectoryInnerDirs)
+        {
+          Walk(d, cmd, filter);
+        }
+      }
+    }
+
+    public void CopyFromTo(string sourceDirName, string destDirName, string filter)
+    {
+      DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+      DirectoryInfo[] dirs = dir.GetDirectories();
+      if(!Directory.Exists(destDirName))
+      {
+        Directory.CreateDirectory(destDirName);
+      }
+
+      FileInfo[] files = dir.GetFiles();
+      foreach(var file in files)
+      {
+        var temppath = Path.Combine(destDirName, file.Name);
+        file.CopyTo(temppath, true);
+      }
+    }
+  }
+}
---
+++ node-flat/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace node_flat
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      if(!Directory.Exists(args[1]))
+      {
+        Console.WriteLine("error: bad source path {0}", args[1]);
+        return;
+      }
+      if(!Directory.Exists(args[2]))
+      {
+        Console.WriteLine("error: bad dest path {0}", args[2]);
+        return;
+      }
+
+      string nodeModulesSrc = args[1];
+      mPackModulesDest = args[2];
+      Command cmd = new Command(Pack);
+
+      try
+      {
+        ExecuteCommand(nodeModulesSrc, cmd);
+      }
+      catch(Exception e)
+      {
+        Console.WriteLine(e.Message);
+      }
+    }
+
+    private static void ExecuteCommand(string nodeModulesSrc, Command cmd)
+    {
+      mNumOfPackedPackages = 0;
+      var myDirUtil = new DirUtils();
+      DirectoryInfo di = new DirectoryInfo(nodeModulesSrc);
+
+      Console.WriteLine("start packing ...");
+
+      myDirUtil.Walk(di, cmd, "node_modules");
+
+      Console.WriteLine("packing ended, packed " + mNumOfPackedPackages + " packages");
+    }
+
+    private static void Pack(DirectoryInfo[] dirs)
+    {
+      int numOfPackagesToPack = dirs.Count();
+      if(numOfPackagesToPack == 0)
+      {
+        return;
+      }
+      mNumOfPackedPackages += numOfPackagesToPack;
+
+      var myDirUtils = new DirUtils();
+      System.Diagnostics.Process process = new System.Diagnostics.Process();
+      System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+      startInfo.WorkingDirectory = mPackModulesDest;
+      startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+      startInfo.FileName = "cmd.exe";
+
+      StringBuilder listOfModules = new StringBuilder();
+      foreach(var d in dirs)
+      {
+        listOfModules.Append(d.FullName + " ");
+        Console.WriteLine("packing " + d.FullName);
+      }
+
+      startInfo.Arguments = "/C npm pack " + listOfModules;
+      process.StartInfo = startInfo;
+      process.Start();
+      process.WaitForExit();
+    }
+
+    private static string mPackModulesDest;
+    private static int mNumOfPackedPackages = 0;
+  }
+}
